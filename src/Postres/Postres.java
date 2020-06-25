d16f9c89d9a817e637f69af0e2a4d9eade08c58f
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2020-06-25 11:06:38 -0500

    Literal 2
    Se elimino la clase OperacionesAderezo
    Se agrego metodos agregar y quitar aderezo a la clase Postre

--- src/Postres/Postres.java
+++ src/Postres/Postres.java
@@ -9,9 +9,26 @@ public class Postres {
 	 protected double precioParcial;
 	 protected ArrayList<Aderezo> aderezos;
 	 
+	
+	 
 	 Postres(String sabor,double precioParcial){
 		 aderezos= new ArrayList<>();
 		 this.sabor = sabor;
 		 this.precioParcial = precioParcial;
 	 }
+	 
+	 public ArrayList<Aderezo> getAderezos() {
+	        return aderezos;
+	    }
+
+	 public static void anadirAderezoPostre(Postres p,Aderezo aderezo){
+	        p.getAderezos().add(aderezo);
+	    }
+
+	   public static void quitarAderezoPostre(Postres p,Aderezo aderezo){
+	        p.getAderezos().remove(aderezo);
+	    }
+	 
+	 
+	   
 }
