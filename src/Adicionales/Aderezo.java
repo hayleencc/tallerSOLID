af5f47ac2a32679276c4a1bd7a20a49ad86a8c67
Hayleen Carrillo <haivcarr@espol.edu.ec>
2020-06-25 11:43:36 -0500

    correccion errores

--- src/Adicionales/Aderezo.java
+++ src/Adicionales/Aderezo.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Adicionales;
 
-/**
- *
- * @author Pedro Mendoza
- */
-public enum Aderezo {
-    MALVAVISCOS,FRUTILLA,CREMA
-    
+public abstract class Aderezo {
+	protected String nombre;
+	
+	public void setNombre(String nom) {
+		this.nombre=nom;
+	}
+	
+	public String toString() {
+		return this.nombre=nombre.toUpperCase();
+	}
+	
 }
